a27b992193cb2847d32de53e0166e9be952ee8ad
Devavrt1234 <devavratsingh2002@gmail.com>
2025-06-13 16:57:23 +0530

    frontend app needs a public url generally to call backned but that can be made private for this you have to use other way

--- Application-Code/frontend/src/services/taskServices.js
+++ Application-Code/frontend/src/services/taskServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl =  "http://localhost:8080/api/tasks";
+const apiUrl =  "http://a71de30d783844e20b031890b39a67d9-1036758062.us-east-1.elb.amazonaws.com:8080/api/tasks";
 console.log(apiUrl)
 export function getTasks() {
     return axios.get(apiUrl);
@@ -16,3 +16,5 @@ export function updateTask(id, task) {
 export function deleteTask(id) {
     return axios.delete(apiUrl + "/" + id);
 }
+
+
